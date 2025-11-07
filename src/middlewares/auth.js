529d969ea504a6f20fe92d03950b7dddacb483d3
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-07 12:39:55 +0600

    Add authentication middleware using JWT

---
+++ src/middlewares/auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const auth = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ 
+        status: "error",
+        message: "Invalid token",
+        details: error.message
+    });
+  }
+};
