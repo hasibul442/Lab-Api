976fae0ddfc482901466c977d383e030f2a4abd2
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-07 21:36:08 +0600

    Add user type routes with CRUD operations for managing user types

--- src/controllers/UserTypeController.js
+++ src/controllers/UserTypeController.js
@@ -32,3 +32,68 @@ export async function createUserType(req, res) {
     res.status(500).json(handleErrorResponse(error));
   }
 }
+
+export async function getUserTypeById(req, res) {
+  try {
+    const { id } = req.params;
+    const userType = await UserType.findById(id);
+    if (!userType) {
+      return res.status(404).json({
+        success: false,
+        message: "User type not found",
+      });
+    }
+    res.status(200).json({
+      data: userType,
+      success: true,
+      message: "User type fetched successfully",
+    });
+  } catch (error) {
+    res.status(500).json(handleErrorResponse(error));
+  }
+}
+
+export async function updateUserType(req, res) {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const updatedUserType = await UserType.findByIdAndUpdate(
+      id,
+      { name, description },
+      { new: true }
+    );
+    if (!updatedUserType) {
+      return res.status(404).json({
+        success: false,
+        message: "User type not found",
+      });
+    }
+    res.status(200).json({
+      data: updatedUserType,
+      success: true,
+      message: "User type updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json(handleErrorResponse(error));
+  }
+}
+
+export async function deleteUserType(req, res) {
+  try {
+    const { id } = req.params;
+    const deletedUserType = await UserType.findByIdAndDelete(id);
+    if (!deletedUserType) {
+      return res.status(404).json({
+        success: false,
+        message: "User type not found",
+      });
+    }
+    res.status(200).json({
+      data: deletedUserType,
+      success: true,
+      message: "User type deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json(handleErrorResponse(error));
+  }
+} 
--- src/routes/userTypeRoutes.js
+++ src/routes/userTypeRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { createUserType, getUserTypes } from '../controllers/UserTypeController.js';
+import { createUserType, deleteUserType, getUserTypeById, getUserTypes, updateUserType } from '../controllers/UserTypeController.js';
 
 const router = express.Router();
 
 router.get("/", getUserTypes);
 router.post("/", createUserType);
+router.get("/:id", getUserTypeById);
+router.put("/:id", updateUserType);
+router.delete("/:id", deleteUserType);
 
 export default router;
