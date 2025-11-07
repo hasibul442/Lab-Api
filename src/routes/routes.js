f13413b9e9e799eb71dd9c9decd07c0110a86894
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-07 21:24:38 +0600

    Refactor routes: streamline route imports and remove unused route handlers

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,51 +1,25 @@
 import express from "express";
 import { healthCheck } from "../controllers/HealthController.js";
 import { homeData } from "../controllers/HomeController.js";
-import { deleteRoleData, getRole, getRoles, storeRole, updateRoleData } from "../controllers/RoleController.js";
-import { createTest, deleteTestData, getTest, getTestList, updateTestData } from "../controllers/TestController.js";
-import { createTestCategory, deleteTestCategory, getTestCategory, getTestCategoryList, updateTestCategory } from "../controllers/TestCategoryController.js";
-import { createEducation, deleteEducation, getEducation, getEducationById, updateEducation } from "../controllers/EducationController.js";
 import { createSpecialty, deleteSpecialtyData, getSpecialties, getSpecialty, updateSpecialtyData } from "../controllers/SpecialitiesController.js";
 import unitRoutes from "./unitRoutes.js";
 import userTypeRoutes from "./userTypeRoutes.js";
+import roleRoutes from "./roleRoutes.js";
+import testRoutes from "./testRoutes.js";
+import educationRoutes from "./educationRoutes.js";
+import specialtiesRoutes from "./specialtiesRoutes.js";
 
 const router = express.Router();
 
 router.get("/health", healthCheck);
-
 router.get("/home", homeData);
 
 router.use('/user-types', userTypeRoutes);
 router.use('/unit', unitRoutes);
+router.use('/role', roleRoutes);
+router.use('/test', testRoutes);
+router.use("/settings/education", educationRoutes);
+router.use("/settings/specialties", specialtiesRoutes);
 
-router.get("/role", getRoles);
-router.post("/role", storeRole);
-router.get("/role/:id", getRole);
-router.put("/role/:id", updateRoleData);
-router.delete("/role/:id", deleteRoleData);
-
-router.get("/test", getTestList);
-router.post("/test", createTest);
-router.get("/test/data/:id", getTest);
-router.put("/test/update/:id", updateTestData);
-router.delete("/test/delete/:id", deleteTestData);
-
-router.get("/test/category", getTestCategoryList);
-router.post("/test/category", createTestCategory);
-router.get("/test/category/:id", getTestCategory);
-router.put("/test/category/:id", updateTestCategory);
-router.delete("/test/category/:id", deleteTestCategory);
-
-router.get("/settings/education", getEducation);
-router.post("/settings/education", createEducation);
-router.get("/settings/education/:id", getEducationById);
-router.put("/settings/education/:id", updateEducation);
-router.delete("/settings/education/:id", deleteEducation);
-
-router.get("/settings/specialties", getSpecialties);
-router.post("/settings/specialties", createSpecialty);
-router.get("/settings/specialties/:id", getSpecialty);
-router.put("/settings/specialties/:id", updateSpecialtyData);
-router.delete("/settings/specialties/:id", deleteSpecialtyData);
 
 export default router;
