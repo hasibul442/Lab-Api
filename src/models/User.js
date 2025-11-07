a551772c710abee64b8f122f470d5f28cdb31601
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-07 12:50:51 +0600

    Refactor User schema to include phone and improve formatting

--- src/models/User.js
+++ src/models/User.js
@@ -1,23 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { 
-    type: String, 
-    required: [ true, 'Email is required' ], 
-    unique: [ true, 'Email must be unique' ],
-    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address']
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email must be unique"],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: [true, "Phone number must be unique"],
+    },
+    password: { type: String, required: true },
+    profileImage: { type: String, required: true },
+    address: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
 
+    role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+    userType: { type: mongoose.Schema.Types.ObjectId, ref: "UserType" },
+    unit: { type: mongoose.Schema.Types.ObjectId, ref: "Unit" },
   },
-  phone: { 
-    type: String, 
-    required: [ true, 'Phone number is required' ], 
-    unique: [ true, 'Phone number must be unique' ], 
-  },
-  password: { type: String, required: true },
-  profileImage: { type: String, required: true },
-  address: { type: String, required: true },
-  isActive: { type: Boolean, default: true },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
